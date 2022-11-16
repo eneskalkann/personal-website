bc9463f798b1f390eaa6a74a324d33294291bdde
eneskalkann <eneskalkan36e@gmail.com>
2022-11-17 00:23:46 +0300

    create data.js

---
+++ src/data.js
@@ -0,0 +1,118 @@
+export const Information = [
+    {
+        "id" : 1,
+        "imageURL": "https://avatars.githubusercontent.com/u/75678744?v=4" ,
+        "title" : "Muhammet Enez Kalkan",
+        "position" : "FrontEnd Developer",
+        "about" : "Hi, my name is Enes. I am student of Akdeniz University, Management Information Systems. I am going to graduate this academic year. I’m interested in being FrontEnd Developer and I am going to improve myself in this way. I am at a nice level in the languages following; HTML , CSS ,JavaScript and React."
+    }
+]
+
+export const Skills = [
+    {
+        "id" : 1,
+        "language" : "HTML",
+        "image" : require("./assets/icons8-html-5.svg")
+    },
+    {
+        "id" : 2,
+        "language" : "CSS",
+        "image" : require("./assets/icons8-css3.svg")
+    },
+    {
+        "id" : 3,
+        "language" : "Javascript",
+        "image" : require("./assets/icons8-javascript.svg")
+    },
+    {
+        "id" : 4,
+        "language" : "Scss",
+        "image" : require("./assets/icons8-sass.svg")
+    },
+    {
+        "id" : 5,
+        "language" : "React",
+        "image" : require("./assets/icons8-react.svg")
+    },
+    {
+        "id" : 6,
+        "language" : "Material UI",
+        "image" : require("./assets/icons8-material-ui.svg")
+    }
+]
+
+export const Projects = [
+    {
+        "id" : 1,
+        "image": "https://user-images.githubusercontent.com/75678744/195865576-093a91f2-2963-49ca-b609-0b2b08e4d83a.png",
+        "title": "Space Tourism Multi Page Website",
+        "urlDemo": "https://react-space-tourism-eneskalkann.vercel.app/", 
+        "urlRepo":"https://github.com/eneskalkann/react-space-tourism" ,
+    },
+    {
+        "id" : 2,
+        "image": "https://user-images.githubusercontent.com/75678744/169774983-606366a5-71f1-4ab5-9637-4b75e55dca26.png",
+        "title": "Expenses Chart Component",
+        "urlDemo": "https://eneskalkann-expenses-chart-component.vercel.app/", 
+        "urlRepo":"https://github.com/eneskalkann/expenses-chart-component" ,
+    },
+    {
+        "id" : 3,
+        "image": require('./assets/loopstudio.png'),
+        "title": "Loopstudio Landing Page",
+        "urlDemo": "https://loopstudio-landing-page-iota.vercel.app/", 
+        "urlRepo":"https://github.com/eneskalkann/loopstudio-landing-page" ,
+    },
+    {
+        "id" : 4,
+        "image": "https://user-images.githubusercontent.com/75678744/196408358-77ef7a76-ed1c-4453-b4ac-bb93e2d9a75b.png",
+        "title": "Unifeed Simple Blog Page",
+        "urlDemo": "https://simple-react-unifeed-blog-app.vercel.app/", 
+        "urlRepo":"https://github.com/eneskalkann/simple-react-unifeed-blog-app" ,
+    },
+    {
+        "id" : 5,
+        "image":  "https://user-images.githubusercontent.com/75678744/195316840-d14fd15b-cbe6-4523-9dc2-908157802cd9.png",
+        "title":  "Social Media Dashboard",
+        "urlDemo": "https://react-dark-mode-social-media.vercel.app/", 
+        "urlRepo":"https://github.com/eneskalkann/react-dark-mode-social-media" ,
+    },
+    {
+        "id" : 6,
+        "image": "https://user-images.githubusercontent.com/75678744/198697365-f16abc88-f777-45c0-b6bf-8c1dfd29898a.png",
+        "title":  "Advice Generator App",
+        "urlDemo": "https://advice-generator-app-eneskalkann.vercel.app/", 
+        "urlRepo":"https://github.com/eneskalkann/advice-generator-app" ,
+    },
+]
+
+export const SocialMedia =[
+    {
+        "id" : 1,
+        "icon": require('./assets/social-media/icons8-instagram.svg'),
+        "url":"https://www.instagram.com/eneskalkan/"
+    },
+    {
+        "id" : 2,
+        "icon": require('./assets/social-media/icons8-linkedin-circled.svg'),
+        "url":"https://www.linkedin.com/in/enes-kalkann/"
+    },
+    {
+        "id" : 3,
+        "icon": require('./assets/social-media/icons8-spotify.svg'),
+        "url":"https://open.spotify.com/user/35oykqp0galk5aif7hv6tfso2"
+    },
+    {
+        "id" : 4,
+        "icon": require('./assets/social-media/icons8-github.svg'),
+        "url":"https://github.com/eneskalkann"
+    }
+]
+
+export const Contact = [
+    {
+        "id" : 1,
+        "title" : "Contact Me",
+        "url" : "https://mail.google.com/mail/?view=cm&fs=1&to=eneskalkan36e@gmail.com&su=Subject&body=&bcc="
+    }
+]
