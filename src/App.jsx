a4710132e853a42932411b20648a392cda103489
eneskalkann <eneskalkan36e@gmail.com>
2022-11-17 00:24:16 +0300

    tried-datajs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import {Information} from './data'
+
 function App() {
 
   return (
     <div className="App">
-      
+      {Information && Information.map(home => {
+        return(
+          <img src={home.imageURL} alt="" />
+        )
+      })}
     </div>
   )
 }
