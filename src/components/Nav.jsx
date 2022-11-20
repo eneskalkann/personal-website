aaa8271a6c9751225805b76ebc7bee7a05461486
eneskalkann <eneskalkan36e@gmail.com>
2022-11-20 22:19:40 +0300

    import_react-scroll-nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,41 +1,40 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-scroll';
+import '../styles/Nav/Nav.css'
 
 function Nav() {
 
-    const links = [
-        {
-            id:1,
-            items: "Home"
-        },
-        {
-            id:2,
-            items: "About"
-        },
-        {
-            id:3,
-            items: "Skills"
-        },
-        {
-            id:4,
-            items: "Projects"
-        },
-        {
-            id:5,
-            items: "Contact"
-        }
-    ]
-
   return (
     <div className='menu'>
-        <div className='nav'>
+        <nav className='nav'>
             <ul className='items'>
-                {links.map(({id, items})=> (
-                    <li key={id}>
-                    </li>
-                ))}
+                <li className="item">
+                <Link to="Home" spy={true} smooth={true} offset={-100} duration={500}>
+                 Home
+                </Link>
+                </li>
+                <li className="item">
+                <Link to="About" spy={true} smooth={true} offset={-100} duration={500}>
+                 About
+                </Link>
+                </li>
+                <li className="item">
+                <Link to="Skills" spy={true} smooth={true} offset={-100} duration={500}>
+                 Skills
+                </Link>
+                </li>
+                <li className="item">
+                <Link to="Projects" spy={true} smooth={true} offset={-100} duration={500}>
+                 Projects
+                </Link>
+                </li>
+                <li className="item">
+                <Link to="Contact" spy={true} smooth={true} offset={-150} duration={500}>
+                 Contact
+                </Link>
+                </li>
             </ul>
-        </div>
+        </nav>
     </div>
   )
 }
