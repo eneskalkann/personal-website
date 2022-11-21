7663f1b5f9473a739146a125d9bc09ce19915b62
eneskalkann <eneskalkan36e@gmail.com>
2022-11-21 15:13:29 +0300

    Desktop-design-done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,82 @@
 import React from 'react'
+import { dataProjects } from '../data'
+import styled from 'styled-components'
 
 function Projects() {
   return (
-    <div name="Projects">
-        <h1>•Projects•</h1>
-    </div>
+    <ContainerProjects name="Projects">
+        <span>•  Projects  •</span>
+        <Data>
+        {
+          dataProjects && dataProjects.map(data => {
+            return(
+              <div className='datas' key={data.id}>
+                <img src={data.image} alt="" />
+                <h3>{data.title}</h3>
+                <div className='buttons'>
+                  <button><a href={data.urlDemo} target="_blank">View Demo</a></button>
+                  <button><a href={data.urlRepo}>View on GitHub</a></button>
+                </div>
+              </div>
+            )
+          })
+        }
+        </Data>
+    </ContainerProjects>
   )
 }
+
+const ContainerProjects = styled.div`
+display: flex;
+flex-direction: column;
+position: relative;
+align-items: center;
+span{
+    color: #98ecc7;
+    margin: 90px 0 55px 0;
+    font-size: 22px;
+}
+`
+
+const Data = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: auto;
+  grid-gap: 1rem;
+  text-align: center;
+  .datas{
+    width: 350px;
+    height: 320px;
+    border: 2px solid #98ecc7;
+    border-radius:10px;
+    color: white;
+    .buttons{
+      justify-content: center;
+      display: flex;
+      gap: 10px;
+    }
+    button{
+      padding: 7px 13px;
+      width: 120px;
+      background-color: #98ecc7;
+      border: none;
+      border-radius:4px;
+      a{
+        text-decoration: none;
+        color: #fff;
+        font-weight: 700;
+      }
+    }
+    img{
+    width: 350px;
+    height: 210px;
+    border-top-left-radius:7px;
+    border-top-right-radius:7px;
+   }
+   h3{
+    margin-top: 13px;
+   }
+  }
+`
 
 export default Projects
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import styled from 'styled-components'
+import { dataSkills } from '../data'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <ContainerSkills name="Skills">
+      <span>•  Skills  •</span>
+      <Data>
+      {
+        dataSkills && dataSkills.map(data => {
+          return(
+            <div className='div-border' key={data.id}>
+              <h4>{data.language}</h4>
+              <img key={data.id} src={data.image} alt="" />
+            </div>
+          )
+        })
+      }
+      </Data>
+    </ContainerSkills>
   )
 }
+
+const ContainerSkills = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  span{
+    color: #98ecc7;
+    margin: 70px 0 35px 0;
+    font-size: 22px;
+  }
+`
+
+const Data = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: auto;
+  grid-gap: 1.3rem;
+  justify-content: center;
+  width: 400px;
+  grid: 2;
+  margin: 0;
+  .div-border{
+    border: 2px solid #98ecc7;
+    width: 220px;
+    height: 70px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    :hover{
+      background-color: #98ecc7;
+      transition: .5s;
+    }
+  }
+  h4{
+    font-weight: 700;
+    color: #fff;
+  }
+  img{
+    margin-right: 20px;
+    width:40px;
+  }
+`
 
 export default Skills
