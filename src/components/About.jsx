3952e9b987ad9e715ff898670d1ad02bd42e063f
eneskalkann <eneskalkan36e@gmail.com>
2022-11-21 12:41:33 +0300

    Desktop-about-design-done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Information } from '../data'
 
 function About() {
   return (
-    <div>About</div>
+    <ContainerAbout name="About">
+      {
+        Information && Information.map(About => {
+          return(
+            <div key={About.id}>
+              <img src={About.imageURL} alt="eneskalkan" />
+              <h1>{About.title}</h1>
+              <h4>{About.position}</h4>
+              <p>{About.about}</p>
+            </div>
+          )
+        })
+      }
+    </ContainerAbout>
   )
 }
+
+const ContainerAbout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin-top: -90px;
+  img{
+    position: relative;
+    width: 160px;
+    border-radius: 50%;
+    border: 2px solid #0ee2b4;
+  }
+  h1{
+    font-weight: 700;
+  }
+  h4{
+    font-weight: 600;
+    letter-spacing: 1.2px;
+  }
+  p{
+    max-width: 650px;
+    color: #878787;
+  }
+`
 
 export default About
