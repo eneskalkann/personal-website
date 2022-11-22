49a5a9885bc03f38c89a80fc865e3185354b6204
eneskalkann <eneskalkan36e@gmail.com>
2022-11-22 22:39:36 +0300

    update-duration-and-offset

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,27 +9,27 @@ function Nav() {
         <nav className='nav'>
             <ul className='items'>
                 <li className="item">
-                <Link to="Home" spy={true} smooth={true} offset={-100} duration={500}>
+                <Link to="Home" spy={true} smooth={true} offset={-100} duration={2000}>
                  Home
                 </Link>
                 </li>
                 <li className="item">
-                <Link to="About" spy={true} smooth={true} offset={-100} duration={500}>
+                <Link to="About" spy={true} smooth={true} offset={-100} duration={2000}>
                  About
                 </Link>
                 </li>
                 <li className="item">
-                <Link to="Skills" spy={true} smooth={true} offset={-100} duration={500}>
+                <Link to="Skills" spy={true} smooth={true} offset={-100} duration={2000}>
                  Skills
                 </Link>
                 </li>
                 <li className="item">
-                <Link to="Projects" spy={true} smooth={true} offset={-100} duration={500}>
+                <Link to="Projects" spy={true} smooth={true} offset={-50} duration={2000}>
                  Projects
                 </Link>
                 </li>
                 <li className="item">
-                <Link to="Contact" spy={true} smooth={true} offset={-150} duration={500}>
+                <Link to="Contact" spy={true} smooth={true} offset={-150} duration={2000}>
                  Contact
                 </Link>
                 </li>
