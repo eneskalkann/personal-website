39fa40848dad3a668b2be16048644b1eeb7ec4c7
eneskalkann <eneskalkan36e@gmail.com>
2022-11-23 17:27:22 +0300

    add-ScrollToTop.js-for-mobile

---
+++ src/hook/ScrollToTop.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+import {IoIosArrowDropup} from 'react-icons/io'
+
+function ScrollToTop() {
+    const scrollTop = () => {
+        window.scrollTo({
+            top:0,
+            behavior:'smooth',
+            duration :'2000'
+        });
+    };
+  return (
+    <ContainerScroll>
+        <a onClick={scrollTop}>Back to top <IoIosArrowDropup className='icon' /></a>
+    </ContainerScroll>
+  )
+}
+
+const ContainerScroll = styled.div`
+    display: none;
+    @media screen and (max-width:700px){
+        display: flex;
+        a{
+            color: #fff;
+            display: flex;
+            align-items: center;
+            font-size: 21px;
+            padding-bottom: 5px;
+            border-bottom: 2px solid #98ecc7;
+            margin-bottom: 80px;
+            .icon{
+                color: #98ecc7;
+                margin-left: 8px;
+            }
+        }
+    }
+`
+
+export default ScrollToTop
